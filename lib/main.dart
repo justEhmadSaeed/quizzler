227a2c5b87c5f1edf775c7843ebcac1c32483533
Ehmad <justEhmadSaeed@gmail.com>
2020-04-21 23:32:15 +0500

    👌 IMPROVE: Code Abstraction

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz-brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -26,7 +29,8 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List questionBank = [];
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +43,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question(String ques, bool ans) {
+    questionText = ques;
+    questionAnswer = ans;
+  }
+}
