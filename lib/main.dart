a48278aafdcbaf0e30831779810ee5c77ebba192
Ehmad <justEhmadSaeed@gmail.com>
2020-04-21 19:03:25 +0500

    📖 DOC: Update README

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,8 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List questionBank = [];
   @override
   Widget build(BuildContext context) {
     return Column(
